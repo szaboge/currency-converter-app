78cfea668b55147bff086bbd01bb7382cbdea921
Szabo Gergely Andras <szagege@gmail.com>
2018-06-20 22:44:52 +0200

    final commit

--- src/app/currencylist/currency/currency.component.ts
+++ src/app/currencylist/currency/currency.component.ts
@@ -21,7 +21,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 })
 
 export class CurrencyComponent implements OnInit {
-  @Input('CurrencyObject') currency: {};
+  @Input('CurrencyObject') currency: { name: string, rate: number, imagePath: string };
 
   constructor() {
   }
